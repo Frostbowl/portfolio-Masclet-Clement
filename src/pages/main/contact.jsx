4af12b9513646cb74c1b5bc7d31462cfc784755f
Frostbowl <masclet.c@gmail.com>
2024-04-18 14:11:32 +0400

    creation page contact jsx & css

---
+++ src/pages/main/contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './contact.css';
+
+const contact = () =>{
+    return(
+        <main>
+            <div className="container">
+                <h1>Me contacter</h1>
+                <h2>Pour me contacter en vue d'un éventuel entretien ou d'une future collaboration, 
+                    merci de remplir le formulaire de contact
+                </h2>
+                <div className="container contact">
+                    <section className=""></section>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default contact;
