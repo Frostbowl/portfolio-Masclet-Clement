903309cab4291c7d3b12086156a07193c0f1cb31
Frostbowl <masclet.c@gmail.com>
2024-04-18 13:48:16 +0400

    final homepage + footer

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './footer.css'
 
 const Footer = () => {
     return(
         <footer>
-            <h1>Footer</h1>
+            <div className="container liens">
+                <div className="row">
+                    <article className="col-md-4">
+                        <h3>Clément Masclet</h3>
+                        <address>
+                            183 Rue du général Bonnier <br />
+                            97430 Le Tampon, La Réunion <br />
+                            <a href="tel:+262692400232">06.92.40.02.32</a><br />
+                            <a href="mailto:masclet.c@gmail.com">masclet.c@gmail.com</a>
+                        </address>
+                    </article>
+                    <article className="col-md-4">
+                        <h3>Liens utiles</h3>
+                        <Link to="/">Accueil</Link>
+                        <Link to="/">Mes réalisations</Link>
+                        <Link to="/">Me contacter</Link>
+                    </article>
+                    <article className="col-md-4">
+                        <h3>Mes réalisations</h3>
+                        <Link to="/">Réalisations</Link>
+                        <Link to="/">Réalisations</Link>
+                        <Link to="/">Réalisations</Link>
+                        <Link to="/">Réalisations</Link>
+                    </article>
+                </div>
+            </div>
+            <div className="copyright">
+                &copy; Designed by <Link to="/">Clément Masclet</Link>
+            </div>
         </footer>
     )
 }
